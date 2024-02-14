2f3b75c61fcd7454c57d3331a2cdbbd088877c52
Vanh1811 <anhnguyenviet065@gmail.com>
2024-02-14 23:26:49 +0800

    react-library-test

---
+++ react-testing/src/App.test.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+  });
+});
